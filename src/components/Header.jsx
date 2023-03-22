53d3594142a282d6242a4d8a3118efeb8ad8ff7d
Kushagra Jain <kushjain1998@gmail.com>
2023-03-22 09:23:27 +0530

    create the spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
     }
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
@@ -34,25 +34,24 @@ export default function Header() {
         <div>
           <ul className="flex space-x-10">
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/") && "text-black border-b-red-500"
+              className={`cursor-pointer py-3 text-sm font-semibold  border-b-[3px]  ${
+                pathMatchRoute("/") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"
               }`}
               onClick={() => navigate("/")}
             >
               Home
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
+                pathMatchRoute("/offers") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"
               }`}
               onClick={() => navigate("/offers")}
             >
               Offers
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
-                "text-black border-b-red-500"
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"
               }`}
               onClick={() => navigate("/profile")}
             >
